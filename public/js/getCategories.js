387a0202724d7a895049de27a9506bfa01837170
kennedy <kennkenn0715@gmail.com>
2024-04-16 13:38:36 -0400

    get category images

--- public/js/getCategories.js
+++ public/js/getCategories.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         categories.forEach((categories) => {
           categoriesContainer.innerHTML += `
               <div>
+                  <img src="${categories.image_url}" alt="${categories.name}">
                   <h3>${categories.name}</h3>
                   <button onclick="window.location.href='/products.html?category=${categories.category_id}'">Shop ${categories.name}</button>
               </div>
