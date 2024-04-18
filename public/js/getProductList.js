0462f6bd42fe8cb5c96a59ed3391bec5e1495b85
kennedy <kennkenn0715@gmail.com>
2024-04-18 01:02:24 -0400

    little tweak

--- public/js/getProductList.js
+++ public/js/getProductList.js
@@ -58,7 +58,6 @@ document.addEventListener("DOMContentLoaded", function () {
                   <div class="product">
                       <img src="${product.image_url}" alt="${product.name}">
                       <h3>${product.name}</h3>
-                      <p>${product.description}</p>
                       <p><strong>Price:</strong> $${product.price.toFixed(
                         2
                       )}</p>
